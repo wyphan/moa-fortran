07622c38f1c4b151462763109d06d9f02bdbcb7a
Wileam Phan <wphan@vols.utk.edu>
2021-11-10 15:44:18 -0500

    Split mod_testing into separate f90 and fypp files

---
+++ tests/mod_testing.f90
@@ -0,0 +1,13 @@
+!===============================================================================
+! Module for testing infrastructure
+! Last edited: Oct 26, 2021 (WYP)
+!===============================================================================
+MODULE mod_testing
+  IMPLICIT NONE
+
+  PRIVATE
+  PUBLIC :: nerr
+
+  INTEGER :: nerr
+
+END MODULE mod_testing
